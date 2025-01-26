0800302f43a801fc9057ffe952cc67b63ef4fede
MD TARIQUL ISLAM <tariqul.developer@gmail.com>
2025-01-26 09:22:58 +0600

    union type done

---
+++ user-defiend-type/union-type.js
@@ -0,0 +1,8 @@
+var userId;
+userId = 1;
+userId = "2";
+var displayUserInfo = function (userId) {
+    console.log(userId);
+};
+displayUserInfo(25);
+displayUserInfo("236");
---
+++ user-defiend-type/union-type.ts
@@ -0,0 +1,11 @@
+let userId: string | number;
+
+userId = 1;
+userId = "2"
+
+const displayUserInfo = (userId: string | number) => {
+    console.log(userId);
+};
+
+displayUserInfo(25)
+displayUserInfo("236")
